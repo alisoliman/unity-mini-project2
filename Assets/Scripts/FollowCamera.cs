3f8956e1515db1b81c1d72a941d0fad6e3deed1c
Ali Soliman <ali.soliman95@gmail.com>
2016-11-16 18:33:48 +0200

    Finalize Scene

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -1,26 +1,1 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class FollowCamera : MonoBehaviour {
-
-    public GameObject target;
-    public float rotateSpeed = 5;
-    Vector3 offset;
-    public float damping = 1;
-
-    void Start() {
-        offset = target.transform.position - transform.position;
-    }
-
-    void LateUpdate() {
-        float currentAngle = transform.eulerAngles.y;
-        float desiredAngle = target.transform.eulerAngles.y;
-        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
-
-        Quaternion rotation = Quaternion.Euler(0, angle, 0);
-        transform.position = target.transform.position - (rotation * offset);
-
-        transform.LookAt(target.transform);
-    }
-
-}
+﻿using UnityEngine;public class FollowCamera : MonoBehaviour {    public GameObject target;    public float rotateSpeed = 5;    Vector3 offset;    public float damping = 1;    void Start() {        offset = transform.position - target.transform.position;    }    void LateUpdate ()    {        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.        transform.position = target.transform.position + offset;    }}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,45 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Player : MonoBehaviour {
-    public float movementSpeed = 10;
+    private float movementSpeed = 0;
+    private Animator anim;
     public float turningSpeed = 60;
 
-    void Update() {
-        float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
-        transform.Rotate(0, horizontal, 0);
 
-        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-        transform.Translate(0, 0, vertical);
+    // Use this for initialization
+    void Start () {
+        anim =  GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.W)) {
+            movementSpeed += .1f;
+            anim.SetBool("WalkBack", false);
+        }
+        else {
+            if (Input.GetKey(KeyCode.S)){
+                movementSpeed += .1f;
+                anim.SetBool("WalkBack", true);
+
+            }
+            else {
+                if (movementSpeed > 0) {
+                    movementSpeed -= .2f;
+                }
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+          transform.Translate(Vector3.forward* Time.deltaTime);
+            transform.Rotate(Vector3.up *150 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+          transform.Translate(Vector3.forward* Time.deltaTime);
+            transform.Rotate(Vector3.down *150 * Time.deltaTime);
+        }
+
+
+        anim.SetFloat("speed", movementSpeed);
     }
 }
